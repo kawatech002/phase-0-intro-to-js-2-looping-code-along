9117fe83e33b0a8f9c644366cb554fe6633f6748
kawatech002 <kawanjiku94@gmail.com>
2023-10-05 23:40:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function countDown(n) {
+    let i = n;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+
+  function writeCards(names) {
+    let thankYouMessages = [];
+    for (let name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  let names = ["Guadalupe", "Ollie", "Aki"];
+  let  thankYouMessages = writeCards(names);
+  
+  console.log(thankYouMessages);
